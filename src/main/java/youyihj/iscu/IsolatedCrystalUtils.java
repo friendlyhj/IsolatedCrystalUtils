5ae4ee07d4f41c259db460d89d21820281cdc43e
youyihj <youyi580@qq.com>
2020-09-24 13:04:06 +0800

    d

--- src/main/java/youyihj/iscu/IsolatedCrystalUtils.java
+++ src/main/java/youyihj/iscu/IsolatedCrystalUtils.java
@@ -7,7 +7,7 @@ public class IsolatedCrystalUtils
 {
     public static final String MODID = "isolatedcrystalutils";
     public static final String NAME = "Isolated Crystal Utils";
-    public static final String VERSION = "1.0.0";
-    public static final String DEPENDENCIES = "require-after:prodigytech";
+    public static final String VERSION = "1.0";
+    public static final String DEPENDENCIES = "required-after:prodigytech";
 
 }
